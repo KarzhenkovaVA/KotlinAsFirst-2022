bd45a91e7c38a4c6d83dc0990ce5e7fef651c1c7
Viktoriia Karzhenkova <karzhenkova.va@edu.spbstu.ru>
2022-12-10 19:04:27 +0300

    lesson 7

--- test/lesson7/task1/Tests.kt
+++ test/lesson7/task1/Tests.kt
@@ -235,6 +235,10 @@ Basic, Ruby, Swift.
         chooseLongestChaoticWord("input/chaotic_in1.txt", "temp.txt")
         assertFileContent("temp.txt", "Карминовый, Некрасивый")
         File("temp.txt").delete()
+
+        chooseLongestChaoticWord("input/chaotic_in2.txt", "temp.txt")
+        assertFileContent("temp.txt", "")
+        File("temp.txt").delete()
     }
 
 
